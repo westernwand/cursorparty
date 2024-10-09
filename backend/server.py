d31a237261a7d496995f15cc8724ce9f83d48653
westernwand <22216855+westernwand@users.noreply.github.com>
2024-10-08 20:52:21 -0400

    changing server->client message structure and file formatting

--- backend/server.py
+++ backend/server.py
@@ -2,17 +2,20 @@ import asyncio
 import websockets
 import json
 
-# generic exception to throw when clients send bad data
+
 class IllegalMessageException(Exception):
+    """ Generic exception to throw when clients send bad data """
     pass
+
 
 # create set for current connections
 connections = set()
 # create dict for mouse positions
 mouse_positions = {}
 
-# initial registration and closure handling
+
 async def register(websocket):
+    """ Handles initial registration and websocket closure """
     print(f"new connection received, id={websocket.id}")
     # add connection to connections set
     connections.add(websocket)
@@ -35,30 +38,35 @@ async def register(websocket):
             print(f"connection removed, id={websocket.id}")
             break
 
-# handle and error check messages from clients
+
 def handle_message(message, websocket):
+    """ Handle and error check messages from clients """
     try:
         tmp = json.loads(message)
         mouse_positions[websocket.id] = (float(tmp["x"]), float(tmp["y"]))
     except:
-        print(f"Illegal message received from {websocket.id}, closing connection\n\t{message}")
+        print(f"Illegal message received from {
+              websocket.id}, closing connection\n\t{message}")
         raise IllegalMessageException
 
-# log current application state
+
 def log_application_state():
+    """ Prints current application state to stdout """
     output = f"{len(connections)} connections: "
     for websocket in connections:
-        output += f"\n\t{websocket.id} - {websocket.remote_address[0]}:{websocket.remote_address[1]} - ({mouse_positions[websocket.id][0]},{mouse_positions[websocket.id][0]})"
+        output += f"\n\t{websocket.id} - {websocket.remote_address[0]}:{
+            websocket.remote_address[1]} - ({mouse_positions[websocket.id][0]},{mouse_positions[websocket.id][0]})"
     print(output)
 
-# sends a mouse connection update to each active, connected client
+
 def broadcast_update():
+    """ Sends the current cursor state to each active, connected client """
     for websocket in connections:
         # skip sending to inactive websockets
         if mouse_positions[websocket.id] == (-1, -1):
             continue
-        # rebuild mouse_positions dict without current mouse position
-        new_dict = {}
+        # create message to send to clients
+        message = []
         for key in mouse_positions:
             # skip if mouse_positions has not been updated yet (indicating inactive connection)
             if mouse_positions[key] == (-1, -1):
@@ -66,11 +74,17 @@ def broadcast_update():
             # skip if sending current websocket's position to current websocket
             if key == websocket.id:
                 continue
-            # add valid positions to new_dict (adding str because json.dumps is having issues with UUIDs as keys)
-            new_dict[str(key)] = mouse_positions[key]
-        # async send message to client (only if new_dict is not empty)
-        if new_dict:
-            asyncio.create_task(websocket.send(json.dumps(new_dict, default=str)))
+            # add mouse position to message
+            message.append({
+                "id": str(key), 
+                "x": mouse_positions[key][0], 
+                "y": mouse_positions[key][1]
+            })
+        # async send message to client (only if message is not empty)
+        if len(message) > 0:
+            asyncio.create_task(websocket.send(
+                json.dumps(message, default=str)))
+
 
 async def main():
     async with websockets.serve(register, "0.0.0.0", 8081):
